625ad726d155604108577d25bdfd5e4282ae8f22
Aryan Yadav <aaryany2505@gmail.com>
2026-07-04 23:50:34 +0530

    added Polaris

--- src/pages/Home2.tsx
+++ src/pages/Home2.tsx
@@ -4,13 +4,19 @@ function Home2() {
   return (
     <div className ={"w-full h-screen relative"}>
     <img src={hero2} className={"w-full h-full object-cover"}/>
-    <div className={"w-full flex h-screen absolute bottom-0 text-end"}>
+    <div className={"w-full  h-screen absolute bottom-0 text-end"}>
         <div className={
-            "w-full bg-yellow-500/40"
+            "w-full h-1/2 "
         }></div>
         <div className={
-            "w-full bg-red-500/40"
-        }></div>
+            "w-full h-1/3 flex justify-between align-center "
+        }>
+          <div className={"flex flex-col  text-white ml-10"}>
+             <h1 className="text-6xl mb-5 ">Polaris</h1>
+            <p className="text-xl">Navigate With Insight</p>
+          </div>
+          
+        </div>
 
     </div>
     </div>
