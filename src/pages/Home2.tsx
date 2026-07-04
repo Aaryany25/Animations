f01c6463d4b71c416ad890b0de1d9573bf31dfa0
Aryan Yadav <aaryany2505@gmail.com>
2026-07-04 23:54:36 +0530

    Completed the Landig Page

--- src/pages/Home2.tsx
+++ src/pages/Home2.tsx
@@ -15,9 +15,22 @@ function Home2() {
              <h1 className="text-6xl mb-5 ">Polaris</h1>
             <p className="text-xl">Navigate With Insight</p>
           </div>
-          
+          <div className={"flex flex-col text-white mr-30"}>
+          <button className={"bg-white text-black px-10 py-5 text-2xl  rounded-4xl mb-5"}>Get Started</button>
+          </div>
         </div>
-
+        <hr></hr>
+         <div className={
+            "w-full h-1/6 flex justify-between align-center "
+        }>
+          <div className={"flex flex-col  text-white ml-10"}>
+             <h1 className="text-6xl mb-5 ">Polaris</h1>
+            <p className="text-xl">Navigate With Insight</p>
+          </div>
+          <div className={"flex flex-col text-white mr-30"}>
+          <button className={"bg-white text-black px-10 py-5 text-2xl  rounded-4xl mb-5"}>Get Started</button>
+          </div>
+        </div>
     </div>
     </div>
   )
