3c4e457b0b71dc8711ee38288e5327d8533a4e94
Aryan Yadav <aaryany2505@gmail.com>
2026-07-11 00:03:27 +0530

    Improved the Card Component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 // import { motion } from 'motion/react'
-import { motion } from "motion/react"
+import { AnimatePresence, motion } from "motion/react"
 
 function Card() {
+ const [open,setOpen] =  useState(true)
   return (
-    <div className="bg-white px-4 py-10 rounded-2xl shadow-xl">
+    <>
+    <AnimatePresence>
+
+    
+    {open ?
+    (<motion.div
+        initial={{opacity:0,scale:0.95}}
+        animate={{opacity:1,scale:1,transition:{duration:0.5}}}
+    exit={{opacity:0,scale:0.95,transition:{duration:0.3}}}
+     className="bg-white px-4 py-10 rounded-2xl shadow-xl">
     <h1 className="text-4xl font-bold text-gray-800 mb-5">Card Title</h1>
     <div className='flex justify-between  items-center mb-5'>
-    <p className="text-gray-600 mb-5 w-full text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, iusto.</p>
-    {/* <Button/> */}
+    <p className="text-gray-600 mb-5 w-1/2 text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, iusto.</p>
+   <button onClick={()=>setOpen(false)} className='text-xl bg-neutral-800 py-2 px-4 rounded-xl text-white '>Close </button>
     </div>
         
     <motion.div 
 initial={{
     opacity:0,
-    scale:0.95,
+    // scale:0.95,
     // filter:"blur(10px)"
 }}
 whileHover={{
@@ -27,7 +37,12 @@ whileHover={{
      className="relative w-full h-64 bg-gray-200 mt-5 rounded-lg border border-dash border-neutral-300">
 <img src="https://i.pinimg.com/control1/736x/15/d1/c6/15d1c6af02826dcd507bab553dc9a1c9.jpg" alt="Card Image" className="w-full h-full object-cover rounded-lg absolute" />
     </motion.div>
-    </div>
+    </motion.div>):
+
+<button onClick={()=>setOpen(true)} className='text-xl bg-neutral-800 py-2 px-4 rounded-xl text-white'>Open</button>
+}</AnimatePresence> 
+    </>
+
   )
 }
 
