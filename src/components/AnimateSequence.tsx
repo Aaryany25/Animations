14b6b721244adc93fe251bff14ba70d45f957a60
Aryan Yadav <aaryany2505@gmail.com>
2026-07-19 21:31:19 +0530

    Added Animation

--- src/components/AnimateSequence.tsx
+++ src/components/AnimateSequence.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React,{useEffect} from 'react'
+import { useAnimate,motion } from 'motion/react'
 function AnimateSequence() {
+  const[scope,animate]=useAnimate()
+  useEffect(()=>{
+StartAnimation()
+  },[])
+  const StartAnimation=()=>{
+    animate("span",{
+opacity:1,
+filter:"blur(0px)"
+    },{
+      duration:1
+    })
+  }
+  const text ="This Line is Gonna Be Animated , this is the Line for Some Animation, This is the Line for Some Animation,  "
   return (
-    <div>Lorem20
-
+    <div ref={scope} className="W-full px-10 text-4xl font-bold text-white bg-black w-full h-screen flex items-center justify-center">
+<motion.span style={{opacity:0,filter:"blur(10px)"}}>{text}</motion.span> 
     </div>
   )
 }
